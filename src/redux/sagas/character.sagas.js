81eda06f1d2bdf4f7c1866d7ca828b703b4abb9f
Jarid Marinos <31664893+Jarid11@users.noreply.github.com>
2021-01-19 22:53:40 -0600

    Update character.sagas.js

--- src/redux/sagas/character.sagas.js
+++ src/redux/sagas/character.sagas.js
@@ -13,13 +13,9 @@ const { getCharacterRequest, getCharacterSuccess, getCharacterFailure } = charac
 function* getCharacterSaga({ payload }) {
     try {
         const { id } = payload
-        // call API
         const response = yield call(getCharacter, id)
-        // using mapper to avoid saving uneeded data in Redux
         const mappedResponse = objectMapper(response, characterMapping)
-        // Wouldn't normaly response of API.. however we want people to see the Spinner
         yield delay(3000)
-        // save data to Redux 
         yield put(getCharacterSuccess(mappedResponse))
     } catch (e) {
         // update Redux with failure
